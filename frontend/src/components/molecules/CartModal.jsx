6ae8251e225a559729d8fd055bf343a7046acbf6
Rony Quispe <oghmand@gmail.com>
2024-04-03 19:10:58 -0500

    add prototype and styles to Cart modal when a product is in it

--- frontend/src/components/molecules/CartModal.jsx
+++ frontend/src/components/molecules/CartModal.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import CartIcon from "../atoms/CartIcon"
+import DeleteButton from "../atoms/DeleteButton"
 
 const CartModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -8,19 +9,29 @@ const CartModal = () => {
     setIsModalOpen(!isModalOpen)
   }
 
-
   return (
     <li className="relative">
       <span className="cursor-pointer relative" onClick={toggleModal}>
         <CartIcon />
       </span>
       {isModalOpen && (
-        <div className="absolute shadow-2xl left-2/4 transform -translate-x-2/4 mt-4 z-10 bg-white">
+        <div className="absolute shadow-2xl left-2/4 transform -translate-x-2/4 mt-4 z-10 bg-white ">
           <div className="title border-b p-4 border-b-gray-300 w-72">
             Cart
           </div>
-          <div className="cart-container flex items-center justify-center min-h-28">
-            Your cart is empty
+          <div className="cart-container flex gap-3 flex-col items-center justify-center min-h-28 p-5">
+            {/* Your cart is empty */}
+            <div className="flex items-center justify-between w-full">
+              <img src="https://via.placeholder.com/50" alt="" />
+              <div>
+                <div>Product name</div>
+                <div>150 x 3 <span>375</span></div>
+              </div>
+              <DeleteButton />
+            </div>
+            <button className="bg-orange-500 w-full text-white rounded-xl p-4">
+              Checkout
+            </button>
           </div>
         </div>
       ) }
